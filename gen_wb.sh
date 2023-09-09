3c65a4b24c5126851436546b315330289f6e27ee
Stafford Horne <shorne@gmail.com>
2023-09-09 21:11:56 +0100

    gen_wb: Make the wishbone generator script work outside my desktop

--- gen_wb.sh
+++ gen_wb.sh
@@ -1,3 +1,22 @@
 #!/bin/sh
 
-~/work/openrisc/orpsoc-cores/cores/wb_intercon/sw/wb_intercon_gen ./data/wb_intercon.conf ./rtl/verilog/wb_intercon.v
+WB_INTERCON_URL=https://raw.githubusercontent.com/openrisc/orpsoc-cores/master/cores/wb_intercon
+
+download() {
+  local url=$1
+  local filename=`basename $url`
+
+  curl $url --output $filename
+}
+
+
+pushd $FILES_ROOT
+  download ${WB_INTERCON_URL}/sw/wb_intercon_gen
+  download ${WB_INTERCON_URL}/sw/verilogwriter.py
+
+  chmod 755 wb_intercon_gen
+
+  ./wb_intercon_gen ./data/wb_intercon.conf ./rtl/verilog/wb_intercon.v
+
+  rm wb_intercon_gen verilogwriter.py verilogwriter.pyc
+popd
