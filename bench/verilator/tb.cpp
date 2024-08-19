097c8160cf9dba9e4535aa5a753f4de6efac4efc
Stafford Horne <shorne@gmail.com>
2024-08-19 10:53:16 +0100

    verilator: Add trace and print support to verilator bench
    The verilator bench did not support instruction tracing as
    it does not include the verilog monitor code used with the icarus bench.
    This patch adds tracing by bringing in some code from or1ksim to handle
    the tracing.
    
    Also, we enable interpreting the nop putc monitor hook to allow printing
    output in verilator even when UART is disabled.

--- bench/verilator/tb.cpp
+++ bench/verilator/tb.cpp
@@ -15,6 +15,7 @@
 #include <signal.h>
 #include <argp.h>
 #include <verilator_tb_utils.h>
+#include <or1ksim_trace.h>
 
 #include "Vorpsoc_top__Syms.h"
 
@@ -32,6 +33,9 @@ static bool done;
 #define NOP_RANDOM		0x000a      /* Return 4 random bytes */
 #define NOP_OR1KSIM		0x000b      /* Return non-zero if this is Or1ksim */
 #define NOP_EXIT_SILENT		0x000c      /* End of simulation, quiet version */
+
+#define SPR_SR_SM          0x00000001  /* Supervisor Mode */
+#define SPR_SR_F           0x00000200  /* Condition Flag */
 
 #define RESET_TIME		2
 
@@ -70,6 +74,7 @@ static int parse_args(int argc, char **argv, VerilatorTbUtils* tbUtils)
 	};
 	struct argp_child child_parsers[] = {
 		{ &verilator_tb_utils_argp, 0, "", 0 },
+		{ &or1ksim_trace_argp, 0, "", 0 },
 		{ 0 }
 	};
 	struct argp argp = { options, parse_opt, 0, 0, child_parsers };
@@ -77,9 +82,82 @@ static int parse_args(int argc, char **argv, VerilatorTbUtils* tbUtils)
 	return argp_parse(&argp, argc, argv, 0, 0, tbUtils);
 }
 
+static uint32_t get_gpr(Vorpsoc_top* top, uint32_t n)
+{
+#ifdef MAROCCHINO
+	return top->orpsoc_top->gencpu__DOT__or1k_marocchino0->u_cpu->get_gpr(n);
+#else
+	return top->orpsoc_top->gencpu__DOT__mor1kx0->mor1kx_cpu->monitor__DOT__get_gpr(n);
+#endif
+}
+
+static void print_trace(Vorpsoc_top* top, uint32_t sr, uint32_t pc, uint32_t insn)
+{
+	char buf[50] = "";
+
+	if (or1ksim_trace_enable ()) {
+		or1ksim_trace (buf, sizeof(buf), insn);
+
+		int trace_dest_reg = or1ksim_trace_dest_reg_get();
+		int trace_src_reg = or1ksim_trace_src_reg_get();
+		int trace_store_addr_reg = or1ksim_trace_store_addr_reg_get();
+		unsigned int trace_store_imm = or1ksim_trace_store_imm_get();
+		int trace_store_val_reg = or1ksim_trace_store_val_reg_get();
+		int trace_store_width = or1ksim_trace_store_width_get();
+		int trace_dest_spr = or1ksim_trace_dest_spr_get();
+
+		printf ("%c ", (sr & SPR_SR_SM) ? 'S' : 'U');
+
+		printf("%08x: %08x %-24s", pc, insn, buf);
+
+		/* Put either the register assignment, SPR value, or store */
+		if (-1 != trace_dest_spr) {
+			printf("SPR[%04x]  = %08x",
+			       trace_dest_spr | get_gpr(top, trace_dest_reg),
+			       get_gpr(top, trace_src_reg));
+		} else if (-1 != trace_dest_reg) {
+			printf("r%-2u        = %08x",
+			       trace_dest_reg,
+			       get_gpr(top, trace_dest_reg));
+		} else {
+			uint32_t store_val = 0;
+			uint32_t store_addr = 0;
+
+			if (0 != trace_store_width) {
+				store_val = get_gpr(top, trace_store_val_reg);
+				store_addr = get_gpr(top, trace_store_addr_reg) +
+					     trace_store_imm;
+			}
+
+			switch (trace_store_width) {
+			case 1:
+				printf("[%08x] = %02x      ", store_addr,
+				       store_val);
+				break;
+			case 2:
+				printf("[%08x] = %04x    ", store_addr,
+				       store_val);
+				break;
+			case 4:
+				printf("[%08x] = %08x", store_addr,
+				       store_val);
+				break;
+			default:
+				printf("                     ");
+				break;
+			}
+		}
+
+		/* Print the flag */
+		printf("  flag: %u\n", sr & SPR_SR_F ? 1 : 0);
+	}
+}
+
 int main(int argc, char **argv, char **env)
 {
 	uint32_t insn = 0;
+	uint32_t adv = 0;
+	uint32_t sr = 0;
 	uint32_t ex_pc = 0;
 
 	Verilated::commandArgs(argc, argv);
@@ -109,15 +187,26 @@ int main(int argc, char **argv, char **env)
 
 #ifdef MAROCCHINO
 		insn = top->orpsoc_top->gencpu__DOT__or1k_marocchino0->u_cpu->monitor_execute_insn;
+		adv = top->orpsoc_top->gencpu__DOT__or1k_marocchino0->u_cpu->monitor_execute_advance;
+		sr = top->orpsoc_top->gencpu__DOT__or1k_marocchino0->u_cpu->monitor_spr_sr;
 		ex_pc = top->orpsoc_top->gencpu__DOT__or1k_marocchino0->u_cpu->monitor_execute_pc;
 #else
 		insn = top->orpsoc_top->gencpu__DOT__mor1kx0->mor1kx_cpu->monitor_execute_insn;
+		adv = top->orpsoc_top->gencpu__DOT__mor1kx0->mor1kx_cpu->monitor_execute_advance;
+		sr = top->orpsoc_top->gencpu__DOT__mor1kx0->mor1kx_cpu->monitor_spr_sr;
 		ex_pc = top->orpsoc_top->gencpu__DOT__mor1kx0->mor1kx_cpu->monitor_execute_pc;
 #endif
-		if (insn == (0x15000000 | NOP_EXIT) || insn == (0x15000000 | NOP_EXIT_SILENT)) {
-			printf("Success! Got NOP_EXIT. Exiting (%lu)\n",
-			       tbUtils->getTime());
-			done = true;
+		if (adv && tbUtils->clkPosEdge(top->wb_clk_i)) {
+			/* Trace instruction to screen if enabled */
+			print_trace(top, sr, ex_pc, insn);
+
+			if (insn == (0x15000000 | NOP_PUTC)) {
+				printf("%c", get_gpr(top, 3));
+			} else if (insn == (0x15000000 | NOP_EXIT) || insn == (0x15000000 | NOP_EXIT_SILENT)) {
+				printf("Success! Got NOP_EXIT. Exiting (%lu)\n",
+				       tbUtils->getTime());
+				done = true;
+			}
 		}
 	}
 
